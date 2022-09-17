7377b728a40624164236cce4822998973e49a4c8
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-09-17 14:43:24 -0600

    time block colors function

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,6 +1,5 @@
-var saveBtnEl = $("saveBtn");
-var textInputEl = $(".plan").text;
-var timeEl = $(".hour");
+var saveBtnEl = $(".saveBtn");
+
 
 
 // add current day and date
@@ -9,44 +8,33 @@ $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
 
 // add save button and when clicked on the text for that event is saved in local storage
-
-// saveBtnEl.on("click", function() {
-// 	here define that text and time 
-// 	and then save it to local storage 
-// 	localStorage.setItem(time, text);
-// 	]
-
 saveBtnEl.on("click", function() {
 
-	var time = timeEl.val();
-	console.log(time);
-	var textInput = textInputEl.val();	
-
-
-
-// keep it from deleting on refesh
-
-
-// save to local storage 
-localStorage.setItem(time, textInput);
-
+	
+	var time = $(this).siblings(".hour").text();
+	var plan = $(this).siblings(".plan").val();	
+	
+	// save to local storage 
+	localStorage.setItem(time, plan);
 });
 
 
-// color code timeblocks / link to css  using movement as current time and past time to indicate past present of future
-// function timeBlockColors() {
-//   let timeBlocks = $(".time-block");
-// 	var today = $.each(".time-block"), function() {
-// 	currentHour = parseInt(timeBlocks.getAttribute("data-time"))
-// 	}
+// color code timeblocks / past present and future
+function timeBlockColors() {
 	
-// 	if(today > currentHour) {
-// 		$(this).addClass(".future");
-// 	} else if (currentHour === today) {
-// 		$(this).addClass(".present");
-// 	}else {
-// 		$(this).addClass(".past");
-// 	}};
+	$(".time-block").each(function () {
+		var timeBlock = $(this).attr('id');
+		
+	
+	var currentHour = moment().hour();
+	if(timeBlock > currentHour) {
+		$(this).addClass(".future");
+	} else if (currentHour === timeBlock) {
+		$(this).addClass(".present");
+	}else {
+		$(this).addClass(".past");
+	}
+})};
 
 
 
