220d03f122c22bbd663d6de00a20a924d797d19d
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-09-17 15:26:37 -0600

    colorblocks functioning

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,41 +1,37 @@
 var saveBtnEl = $(".saveBtn");
 
-
-
 // add current day and date
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
-
 // add save button and when clicked on the text for that event is saved in local storage
 saveBtnEl.on("click", function() {
 
-	
 	var time = $(this).siblings(".hour").text();
 	var plan = $(this).siblings(".plan").val();	
 	
 	// save to local storage 
 	localStorage.setItem(time, plan);
-});
 
+	console.log("plan")
+	if(!localStorage.getItem("plan") || JSON.parse(localStorage.getItem("plan")).length === 0){
+		$window.localStorage.setItem("plan", JSON.stringify($scope.plan));
+}});
 
 // color code timeblocks / past present and future
 function timeBlockColors() {
 	
 	$(".time-block").each(function () {
-		var timeBlock = $(this).attr('id');
-		
+		var timeBlock = $(this).attr("id");
 	
 	var currentHour = moment().hour();
 	if(timeBlock > currentHour) {
-		$(this).addClass(".future");
-	} else if (currentHour === timeBlock) {
-		$(this).addClass(".present");
+		$(this).addClass("future");
+	} else if (currentHour == timeBlock) {
+		$(this).addClass("present");
 	}else {
-		$(this).addClass(".past");
+		$(this).addClass("past");
 	}
 })};
 
-
-
-// colors to show up/ functional
+timeBlockColors();
